96cc0f0a08e2cfc324998e87b59ff53130829429
RenM23 <148499856+RenM23@users.noreply.github.com>
2024-09-22 16:59:45 -0400

    Test github sucess

--- HeatherMcConnellA5(1)/HeatherMcConnellA5(1)/obj/Debug/net8.0-windows/HeatherMcConnellA5(1).AssemblyInfo.cs
+++ HeatherMcConnellA5(1)/HeatherMcConnellA5(1)/obj/Debug/net8.0-windows/HeatherMcConnellA5(1).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatherMcConnellA5(1)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fad5c57142dba302646bb9baf1e42353a55b82f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatherMcConnellA5(1)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatherMcConnellA5(1)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
